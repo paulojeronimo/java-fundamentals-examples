77a0f10c6250026b1a6fe0727c387d1620ee987d
Paulo Jerônimo <paulojeronimo@gmail.com>
2017-09-13 10:14:06 -0300

    Adição do exemplo TestaPrimitivos

---
+++ oo1/TestaPrimitivos.java
@@ -0,0 +1,14 @@
+public class TestaPrimitivos {
+  public static void main(String args[]) {
+    int a = 10;
+    double b = 11.0;
+    
+    a = (int) b;
+
+    System.out.println(a);
+
+    b = 15;
+    
+    System.out.println(b);
+  }
+}
