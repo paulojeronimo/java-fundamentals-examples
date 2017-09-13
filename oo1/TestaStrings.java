a91fd5a28aefd57c2c96b7b1a1d853420c96f411
Paulo Jerônimo <paulojeronimo@gmail.com>
2017-09-13 10:07:39 -0300

    Adição do exemplo TestaStrings

---
+++ oo1/TestaStrings.java
@@ -0,0 +1,14 @@
+public class TestaStrings {
+  public static void main(String args[]) {
+    String a = "Paulo";
+    String b = "Jeronimo";
+    
+    a = b;
+
+    System.out.println(a);
+
+    b = "Jose";
+    
+    System.out.println(b);
+  }
+}
