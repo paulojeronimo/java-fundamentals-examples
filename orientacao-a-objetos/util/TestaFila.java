fccb5e7bc8816c35841b2b2f0f2a965ee5d9fe30
Paulo Jerônimo <paulojeronimo@gmail.com>
2017-09-14 13:45:41 -0300

    Modificação de TestaFila para "nível pleno"

--- orientacao-a-objetos/util/TestaFila.java
+++ orientacao-a-objetos/util/TestaFila.java
@@ -3,22 +3,27 @@ package util;
 enum Cor { PRETO, AZUL, VERMELHO }
 
 public class TestaFila {
+
+  public static void inserir(Fila fila, Cor cor) {
+    fila.inserir(cor);
+    System.out.println("Inserindo a cor " + cor + " na fila");
+  }
+
+  public static Cor remover(Fila fila) {
+    Cor cor = (Cor) fila.remover();
+    System.out.println("Removendo a cor " + cor + " da fila");
+    return cor;
+  }
+
   public static void main(String[] args) {
     Fila filaDeCores = new Fila(3);
 
-    filaDeCores.inserir(Cor.PRETO);
-    System.out.println("Inserindo a cor " + Cor.PRETO + " na fila");
-
-    filaDeCores.inserir(Cor.AZUL);
-    System.out.println("Inserindo a cor " + Cor.AZUL + " na fila");
-
-    filaDeCores.inserir(Cor.VERMELHO);
-    System.out.println("Inserindo a cor " + Cor.VERMELHO + " na fila");
+    inserir(filaDeCores, Cor.PRETO);
+    inserir(filaDeCores, Cor.AZUL);
+    inserir(filaDeCores, Cor.VERMELHO);
 
     Cor cor = null;
-    while (filaDeCores.temObjects()) {
-      cor = (Cor) filaDeCores.remover();
-      System.out.println("Removendo a cor " + cor + " da fila");
-    }
+    while (filaDeCores.temObjects())
+      cor = remover(filaDeCores);
   }
 }
