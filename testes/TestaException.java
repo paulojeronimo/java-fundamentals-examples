b1f20dab323cdb8aa6a5ab5275680012478a3908
Paulo Jerônimo <paulojeronimo@gmail.com>
2017-09-15 15:48:49 -0300

    Ampliação do exemplo TestaException

--- testes/TestaException.java
+++ testes/TestaException.java
@@ -1,10 +1,28 @@
+class SaldoInsuficiente extends Exception {
+}
+
+class ContaCorrente {
+  double saldo;
+  double limite;
+  ContaCorrente(double saldoInicial, double limite) {
+    saldo = saldoInicial;
+  }
+  void sacar(double valor) throws SaldoInsuficiente {
+    double saldo = this.saldo;
+    saldo -= valor;
+    if (saldo + limite < 0)
+      throw new SaldoInsuficiente();
+    this.saldo = saldo;
+  }
+}
+
 public class TestaException {
 
   public static String stringInicial() {
     return null;
   }
 
-  public static void main(String[] a) {
+  public static void main(String[] args) {
     String s = stringInicial();
 
     int i = 10;
@@ -18,8 +36,27 @@ public class TestaException {
       // ...
 
     } catch (NullPointerException erro) {
-      System.out.println("Opa ... avise ao programador que ele fez uma merdinha ...");
-      System.out.println("O erro foi: " + erro);
+      System.err.println("Opa ... avise ao programador que ele fez uma merdinha ...");
+      System.err.println("O erro foi: " + erro);
+    }
+
+    ContaCorrente cc = new ContaCorrente(10, 2);
+    try {
+      if (args.length != 1) {
+        System.out.println("Favor informar um numero!");
+        System.exit(1);
+      }
+      double valor = Double.parseDouble(args[0]);
+      cc.sacar(valor);
+    } catch (SaldoInsuficiente e) {
+      System.out.println("Saldo insuficiente para o saque!");
+      System.exit(2);
+    } catch (NumberFormatException e) {
+      System.out.println("Favor informar um numero valido!");
+      System.exit(3);
+    } catch (Exception e) {
+      System.err.println("Ocorreu um erro. Verifique!");
+      System.exit(4);
     }
   }
 }
