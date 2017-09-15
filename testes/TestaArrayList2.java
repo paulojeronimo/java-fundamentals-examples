f50432f725818dff1eae506dafa72d154b179f4d
Paulo Jerônimo <paulojeronimo@gmail.com>
2017-09-15 10:46:39 -0300

    Alteração de TestaArrayList2 p/ utilizar objetos Empresa

--- testes/TestaArrayList2.java
+++ testes/TestaArrayList2.java
@@ -1,25 +1,33 @@
 import java.util.ArrayList;
 
+class Empresa {
+  String nome;
+  public Empresa(String nome) { this.nome = nome; }
+  @Override
+  public String toString() { return nome; }
+}
+
 public class TestaArrayList2 {
-  public static void listaEmpresas(ArrayList<String> empresas) {
+  public static void listaEmpresas(ArrayList<Empresa> empresas) {
     System.out.println("Empresas cadastradas:");
-    for(String empresa: empresas)
+    for(Empresa empresa: empresas)
       System.out.println(empresa);
   }
 
   public static void main(String[] a) {
-    ArrayList<String> empresas = new ArrayList<>();
+    ArrayList<Empresa> empresas = new ArrayList<>();
 
-    empresas.add("Oracle");
-    empresas.add("RedHat");
-    empresas.add("IBM");
+    empresas.add(new Empresa("Oracle"));
+    empresas.add(new Empresa("RedHat"));
+    empresas.add(new Empresa("IBM"));
 
-    empresas.add(1, "Pivotal");
+    Empresa pivotal = new Empresa("Pivotal");
+    empresas.add(1, pivotal);
 
     listaEmpresas(empresas);
 
-    if (empresas.contains("Pivotal"))
-      empresas.remove("Pivotal");
+    if (empresas.contains(pivotal))
+      empresas.remove(pivotal);
 
     listaEmpresas(empresas);
   }
