a19d6dfc9fd575db33e45301b38e09e6d36007ed
Paulo Jerônimo <paulojeronimo@gmail.com>
2017-09-15 08:46:22 -0300

    Adição de TestaLocalDate

---
+++ testes/TestaLocalDate.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class TestaLocalDate {
+  public static void main(String[] args) {
+    LocalDate myDate = LocalDate.now();
+    System.out.println(myDate);
+  }  
+}
