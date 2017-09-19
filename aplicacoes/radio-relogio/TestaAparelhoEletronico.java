46df3c0454c93291642c92361ebcda28e01490dc
Paulo Jerônimo <paulojeronimo@gmail.com>
2017-09-19 14:27:28 -0300

    [radio-relogio] Adição de TestaAparelhoEletronico

---
+++ aplicacoes/radio-relogio/TestaAparelhoEletronico.java
@@ -0,0 +1,21 @@
+import interfaces.*;
+
+public class TestaAparelhoEletronico {
+  private AparelhoEletronico aparelho;
+
+  public TestaAparelhoEletronico(AparelhoEletronico aparelho) {
+    this.aparelho = aparelho;
+  }
+
+  public void testaLigar() {
+    aparelho.ligar();
+    System.out.println("O testeLigar " +
+      (aparelho.getEstado()? "passou" : "nao passou!"));
+  } 
+
+  public void testaDesligar() {
+    aparelho.desligar();
+    System.out.println("O testeDesligar " +
+      (!aparelho.getEstado()? "passou" : "nao passou!"));
+  } 
+}
--- aplicacoes/radio-relogio/Teste.java
+++ aplicacoes/radio-relogio/Teste.java
@@ -1,23 +1,10 @@
 import objetos.*;
-import static objetos.RadioRelogio.Brilho.INTENSO;
 
 public class Teste {
-  static void imprimirEstado(interfaces.AparelhoEletronico aparelho) {
-    System.out.println("O aparelho esta " + 
-      (aparelho.getEstado()? "ligado": "desligado"));
-  }
-
   public static void main(String[] args) {
-    interfaces.AparelhoEletronico radioRelogio = new RadioRelogio(new Relogio());
-
-    radioRelogio.ligar();
-    
-    imprimirEstado(radioRelogio);
-   
-    radioRelogio.desligar();
-    //radioRelogio.setBrilho(RadioRelogio.Brilho.INTENSO); <- está ok mas ... huummm :/
-    ((RadioRelogio) radioRelogio).setBrilho(INTENSO); // agora sim!
-    
-    imprimirEstado(radioRelogio);
+    TestaAparelhoEletronico teste =
+      new TestaAparelhoEletronico(new RadioRelogio(new Relogio()));
+    teste.testaLigar();
+    teste.testaDesligar();
   }
 }
