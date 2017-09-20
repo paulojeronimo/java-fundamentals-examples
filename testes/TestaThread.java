f5cca2af3abe533a5ee493312bd835807b096bf6
Paulo Jerônimo <paulojeronimo@gmail.com>
2017-09-20 09:39:45 -0300

    Modicação de TestaThread p/ utilização de uma classe anônima

--- testes/TestaThread.java
+++ testes/TestaThread.java
@@ -1,19 +1,3 @@
-class ContadorLento implements Runnable {
-  public void run() {
-    System.out.println("Estou na Thread ContadorLento");
-    int contador = 0;
-    while (contador++ < 10) {
-      System.out.println("Contador lento: " + contador);
-      try {
-        Thread.sleep(2000);
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    }      
-    System.out.println("Thread ContadorLento acabou");
-  }
-}
-
 public class TestaThread {
   public static void run() {
     int contador = 0;
@@ -24,8 +8,22 @@ public class TestaThread {
   }
 
   public static void main(String args[]) {
-    System.out.println("Estou na Thread principal");
-    Thread t = new Thread(new ContadorLento());
+    System.out.println("Iniciei a Thread principal");
+    Thread t = new Thread(new Runnable() {
+      public void run() {
+        System.out.println("Iniciei a Thread ContadorLento");
+        int contador = 0;
+        while (contador++ < 10) {
+          System.out.println("Contador lento: " + contador);
+          try {
+            Thread.sleep(2000);
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+        }      
+        System.out.println("Thread ContadorLento acabou");
+      }
+    });
     t.start();
     run();
     System.out.println("Thread principal acabou");
