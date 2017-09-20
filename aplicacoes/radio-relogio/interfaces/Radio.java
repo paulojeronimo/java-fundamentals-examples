0ca7ae0b24c93dbc198d948718be3b133e34a002
Paulo Jerônimo <paulojeronimo@gmail.com>
2017-09-20 14:03:51 -0300

    [radio-relogio] Criação de um método default na interface de Radio

--- aplicacoes/radio-relogio/interfaces/Radio.java
+++ aplicacoes/radio-relogio/interfaces/Radio.java
@@ -3,4 +3,8 @@ package interfaces;
 public interface Radio extends AparelhoEletronico {
   void sintonizar(Frequencia frequencia);
   GeneroMusical getGeneroMusical();
+
+  default void sintonizarRadioPreferida() {
+      sintonizar(Frequencia.BrasiliaSuperRadioFM);
+  }
 }
