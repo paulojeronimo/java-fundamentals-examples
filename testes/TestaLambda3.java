9510bc1c809815299bc4493a9ce4092010231002
Paulo Jerônimo <paulojeronimo@gmail.com>
2017-09-20 15:11:27 -0300

    Adição de Analisador em TestaLambda3

--- testes/TestaLambda3.java
+++ testes/TestaLambda3.java
@@ -1,12 +1,20 @@
 import java.util.*;
 
+class Analisador {
+  public boolean analisa(String alvo, String busca) {
+    return alvo.contains(busca);
+  }
+}
+
 public class TestaLambda3 {
   public static void main(String args[]) {
     String[] lista = new String[] { "Paulo", "Jose", "Manuel", "Jair", "Roberio" };
     String busca = "a";
 
+    Analisador ana = new Analisador();
+
     for (String s: lista) {
-      if (s.contains(busca)) {
+      if (ana.analisa(s, busca)) {
         System.out.println(s);
       }
     }
