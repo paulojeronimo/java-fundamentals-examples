52d3c6d6b60bf53cb72e8956e6f838f4d21816c4
Paulo Jerônimo <paulojeronimo@gmail.com>
2017-09-21 12:21:49 -0300

    [Testes] Adições em TestaArrayList2

--- netbeans/Testes/TestaArrayList2.java
+++ netbeans/Testes/TestaArrayList2.java
@@ -1,4 +1,6 @@
+import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 class Empresa {
   String nome;
@@ -7,28 +9,46 @@ class Empresa {
   public String toString() { return nome; }
 }
 
+class EmpresaDeTransporte extends Empresa {
+  public EmpresaDeTransporte(String nome) {
+    super(nome);
+  }
+}
+
 public class TestaArrayList2 {
-  public static void listaEmpresas(ArrayList<Empresa> empresas) {
-    System.out.println("Empresas cadastradas:");
-    for(Empresa empresa: empresas)
-      System.out.println(empresa);
+  public static void listaEmpresas(String mensagem, 
+    List<? extends Empresa> empresas) {
+    System.out.println(mensagem);
+    empresas.forEach(System.out::println);
+    System.out.println();
   }
 
   public static void main(String[] a) {
-    ArrayList<Empresa> empresas = new ArrayList<>();
+    List<Empresa> empresasDeInformatica = new ArrayList<>();
+    empresasDeInformatica.add(new Empresa("Oracle"));
+    empresasDeInformatica.add(new Empresa("RedHat"));
+    empresasDeInformatica.add(new Empresa("IBM"));
+    listaEmpresas("Empresas de informática:", empresasDeInformatica);
 
-    empresas.add(new Empresa("Oracle"));
-    empresas.add(new Empresa("RedHat"));
-    empresas.add(new Empresa("IBM"));
+    List<EmpresaDeTransporte> empresasDeTransporte = 
+      Arrays.asList(
+        new EmpresaDeTransporte("Araguarina"),
+        new EmpresaDeTransporte("TransQualquerCoisa"));
+    listaEmpresas("Empresas de transporte:", empresasDeTransporte);
 
+    List<Empresa> empresas = new ArrayList<>(empresasDeTransporte);
+    empresas.addAll(empresasDeInformatica);
+    listaEmpresas("Todas as empresas:", empresas);
+
+    System.out.println("Adicionando a Pivotal a empresasDeInformatica\n");
     Empresa pivotal = new Empresa("Pivotal");
-    empresas.add(1, pivotal);
+    empresasDeInformatica.add(1, pivotal);
 
-    listaEmpresas(empresas);
+    if (empresasDeInformatica.contains(pivotal)) {
+      System.out.println("Removendo a Pivotal de empresasDeInformatica\n");
+      empresasDeInformatica.remove(pivotal);
+    }
 
-    if (empresas.contains(pivotal))
-      empresas.remove(pivotal);
-
-    listaEmpresas(empresas);
+    listaEmpresas("Empresas de informática:", empresasDeInformatica);
   }
 }
