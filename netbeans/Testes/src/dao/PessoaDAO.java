67c72407ec19afc1359b42c7d740df3ceaff4b0f
Paulo Jerônimo <paulojeronimo@gmail.com>
2017-09-21 14:39:17 -0300

    [Testes] - Adição de PessoaDAO

---
+++ netbeans/Testes/src/dao/PessoaDAO.java
@@ -0,0 +1,22 @@
+package dao;
+
+import entidades.Pessoa;
+
+// CRUD (CREATE, RETRIVE, UPDATE, DELETE) para a classe Pessoa
+public class PessoaDAO {
+    public int criar(Pessoa pessoa) {
+        return 0;
+    }
+    
+    public Pessoa obter(int id) {
+        return null;
+    }
+    
+    public void atualizar(int id, Pessoa pessoa) {
+        
+    }
+    
+    public void remover(Pessoa pessoa) {
+        
+    }
+}
