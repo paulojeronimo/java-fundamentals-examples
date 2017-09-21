18235b4cf61ecf525c08e15a7aadaa4756359880
Paulo Jerônimo <paulojeronimo@gmail.com>
2017-09-21 11:00:28 -0300

    [Testes] - Adição de Predicate(s) ao TestaBuilder

--- netbeans/Testes/src/TestaBuilder.java
+++ netbeans/Testes/src/TestaBuilder.java
@@ -1,28 +1,48 @@
+
 import java.util.List;
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 class Pessoa {
+
     private static List<Pessoa> pessoas = new ArrayList<>();
-    
-    public enum Sexo {MASCULINO, FEMININO};
-    
-    String nome; // obrigatório
-    int idade; // opcional
-    Sexo sexo; // opcional
-    
-    public String getNome(String nome) { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-    
-    public int getIdade() { return idade; }
-    public void setIdade(int idade) { this.idade = idade; }
-    
-    public Sexo getSexo() { return sexo; }
-    public void setSexo(Sexo sexo) { this.sexo = sexo; }
-  
+
+    public enum Sexo {
+        MASCULINO, FEMININO
+    };
+
+    private String nome; // obrigatório
+    private int idade; // opcional
+    private Sexo sexo; // opcional
+
+    public String getNome(String nome) {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+
     private Pessoa(String nome) {
         this.nome = nome;
     }
-    
+
     static Builder Builder(String nome) {
         Pessoa pessoa = new Pessoa(nome);
         pessoas.add(pessoa);
@@ -32,29 +52,34 @@ class Pessoa {
     static List<Pessoa> getList() {
         return pessoas;
     }
-    
+
     static class Builder {
+
         Pessoa pessoa;
-     
+
         public Builder(Pessoa pessoa) {
             this.pessoa = pessoa;
         }
+
         public Builder(String nome) {
             pessoa = new Pessoa(nome);
         }
+
         public Builder sexo(Sexo sexo) {
             pessoa.setSexo(sexo);
             return this;
         }
+
         public Builder idade(int idade) {
             pessoa.setIdade(idade);
             return this;
         }
+
         public Pessoa build() {
             return pessoa;
         }
     }
-    
+
     @Override
     public String toString() {
         return nome;
@@ -62,6 +87,7 @@ class Pessoa {
 }
 
 public class TestaBuilder {
+
     public static void main(String[] args) {
         Pessoa jair = Pessoa.Builder("Jair")
                 .sexo(Pessoa.Sexo.MASCULINO)
@@ -85,14 +111,29 @@ public class TestaBuilder {
                 .idade(43)
                 .sexo(Pessoa.Sexo.MASCULINO)
                 .build();
-        
+
         List<Pessoa> pessoas = Pessoa.getList();
         System.out.println("Todas as pessoas:");
         pessoas.forEach(System.out::println);
-        
+
+        Predicate<Pessoa> pessoasComMaisDe30 = (pessoa) -> pessoa.getIdade() > 30;
+        Predicate<Pessoa> pessoasDoSexoIndefinido = (p) -> p.getSexo() == null;
+        Predicate<Pessoa> pessoasDoSexoFeminino = (p) -> 
+                p.getSexo() != null && p.getSexo().equals(Pessoa.Sexo.FEMININO);
+
         System.out.println("Pessoas com mais de 30 anos:");
         pessoas.stream()
-            .filter((pessoa) -> pessoa.getIdade() > 30)
-            .forEach(System.out::println);
+                .filter(pessoasComMaisDe30)
+                .forEach(System.out::println);
+        
+        System.out.println("Pessoas do sexo indefinido:");
+        pessoas.stream()
+                .filter(pessoasDoSexoIndefinido)
+                .forEach(System.out::println);
+        
+        System.out.println("Pessoas do sexo feminino:");
+        pessoas.stream()
+                .filter(pessoasDoSexoFeminino)
+                .forEach(System.out::println);
     }
 }
