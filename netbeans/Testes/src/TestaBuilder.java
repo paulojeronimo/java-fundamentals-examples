6330ff1321c964d26a19323116964db3340362aa
Paulo Jerônimo <paulojeronimo@gmail.com>
2017-09-21 14:08:45 -0300

    [Testes] - Refactoring em TestaBuilder para separar a classe Pessoa

--- netbeans/Testes/src/TestaBuilder.java
+++ netbeans/Testes/src/TestaBuilder.java
@@ -3,94 +3,13 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.function.Predicate;
 
-class Pessoa {
-
-    private static List<Pessoa> pessoas = new ArrayList<>();
-
-    public enum Sexo {
-        MASCULINO, FEMININO
-    };
-
-    private String nome; // obrigatório
-    private int idade; // opcional
-    private Sexo sexo; // opcional
-
-    public String getNome(String nome) {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public Sexo getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(Sexo sexo) {
-        this.sexo = sexo;
-    }
-
-    private Pessoa(String nome) {
-        this.nome = nome;
-    }
-
-    static Builder Builder(String nome) {
-        Pessoa pessoa = new Pessoa(nome);
-        pessoas.add(pessoa);
-        return new Builder(pessoa);
-    }
-
-    static List<Pessoa> getList() {
-        return pessoas;
-    }
-
-    static class Builder {
-
-        Pessoa pessoa;
-
-        public Builder(Pessoa pessoa) {
-            this.pessoa = pessoa;
-        }
-
-        public Builder(String nome) {
-            pessoa = new Pessoa(nome);
-        }
-
-        public Builder sexo(Sexo sexo) {
-            pessoa.setSexo(sexo);
-            return this;
-        }
-
-        public Builder idade(int idade) {
-            pessoa.setIdade(idade);
-            return this;
-        }
-
-        public Pessoa build() {
-            return pessoa;
-        }
-    }
-
-    @Override
-    public String toString() {
-        return nome;
-    }
-}
+import entidades.Pessoa;
+import static entidades.Pessoa.Sexo.*;
 
 public class TestaBuilder {
-
     public static void main(String[] args) {
         Pessoa jair = Pessoa.Builder("Jair")
-                .sexo(Pessoa.Sexo.MASCULINO)
+                .sexo(MASCULINO)
                 .idade(25)
                 .build();
         Pessoa ze = Pessoa.Builder("José")
@@ -101,15 +20,15 @@ public class TestaBuilder {
                 .build();
         Pessoa roberio = Pessoa.Builder("Roberio")
                 .idade(36)
-                .sexo(Pessoa.Sexo.FEMININO)
+                .sexo(FEMININO)
                 .build();
         Pessoa manuel = Pessoa.Builder("Manuel")
                 .idade(58)
-                .sexo(Pessoa.Sexo.MASCULINO)
+                .sexo(MASCULINO)
                 .build();
         Pessoa paulo = Pessoa.Builder("Paulo")
                 .idade(43)
-                .sexo(Pessoa.Sexo.MASCULINO)
+                .sexo(MASCULINO)
                 .build();
 
         List<Pessoa> pessoas = Pessoa.getList();
