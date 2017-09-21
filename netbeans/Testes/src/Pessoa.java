735668ee60fb7255ecf9342600b0a09d24160e9d
Paulo Jerônimo <paulojeronimo@gmail.com>
2017-09-21 18:30:43 -0300

    [Testes] - Aplicação de design patters a classe Pessoa

--- netbeans/Testes/src/Pessoa.java
+++ netbeans/Testes/src/Pessoa.java
@@ -1,27 +1,62 @@
-public class Pessoa {
-    public static void main(String[] args) {
-        if (args.length < 1 || args.length > 2) {
-            mostrarUtilizacao();
-            System.exit(0);
-        }
-            
-        switch (args[0]) {
-            case "criar": criar(args); break;
-            case "remover": remover(args); break;
-            default:
-                mostrarUtilizacao();
-        }
-    }
+interface Comando {
+   public void executar(String[] args);
+}
 
-    private static void mostrarUtilizacao() {
+class ComandoNaoEncontrado implements Comando {
+    @Override
+    public void executar(String[] args) {
         System.out.println("java Pessoa <criar|obter|remover|listar>");
     }
+}
 
-    private static void criar(String ... args) {
+class ComandoCriar implements Comando {
+    @Override
+    public void executar(String[] args) {
         System.out.println("Criando ...");
     }
+}
 
-    private static void remover(String ... args) {
+class ComandoObter implements Comando {
+    @Override
+    public void executar(String[] args) {
+        System.out.println("Obtendo ...");
+    }
+}
+
+class ComandoRemover implements Comando {
+    @Override
+    public void executar(String[] args) {
         System.out.println("Removendo ...");
+    }
+}
+
+class ComandoListar implements Comando {
+    @Override
+    public void executar(String[] args) {
+        System.out.println("Listando ...");
+    }
+}
+
+public class Pessoa {
+    static Comando getComando(String comando) {
+        if (comando != null && comando.length() > 0) {
+            char[] chars = comando.toCharArray();
+            chars[0] = Character.toUpperCase(chars[0]);
+            comando = String.valueOf(chars);
+            try {
+                return (Comando) Class.forName("Comando" + comando).newInstance();
+            } catch (ClassNotFoundException | IllegalAccessException | InstantiationException ex) {
+                //Logger.getLogger(Pessoa.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return new ComandoNaoEncontrado();
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 1 || args.length > 2) {
+            new ComandoNaoEncontrado().executar(null);
+            System.exit(0);
+        }
+        getComando(args[0]).executar(args);
     }
 }
